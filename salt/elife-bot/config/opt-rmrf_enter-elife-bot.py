969601baac0dbb7ccab07291237e78da5c30ead6
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-29 17:07:28 +0100

    Changing cleanup to /bot-tmp

--- salt/elife-bot/config/opt-rmrf_enter-elife-bot.py
+++ salt/elife-bot/config/opt-rmrf_enter-elife-bot.py
@@ -4,7 +4,7 @@ from rmrf_enter import has_ymd_pattern, older_than_N_days
 def files_to_delete():
     return [
         # 'debug' files that accumulate
-        ("delete", "/tmp/", \
+        ("delete", "/bot-tmp/", \
                 lambda fname: has_ymd_pattern(fname) and older_than_N_days(fname, days={{ pillar.elife_bot.rmrf_enter.days }})),
     ]
 
